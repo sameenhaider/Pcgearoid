f4478eb8ddf4691a491dbb1465b3945d0c5c472f
SameenHaider <sam_hai999@hotmail.com>
2020-08-27 21:37:29 +0500

    commit

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import { RiHeart2Line } from "react-icons/ri";
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
-      <p>All Copyright Reserved by <Link to="/">PC Gearoid</Link></p>
+      <p>All Copyright Reserved by PC Gearoid</p>
     </div>
   </footer>
 )
